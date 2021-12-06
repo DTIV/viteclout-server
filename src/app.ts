dff0cf5bb9ae74dfe04f9770069487392d52b260
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-12-06 20:01:48 +0530

    added express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello");
+import express from "express";
+import config from "./config";
+const { PORT } = config;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App is running at ${PORT}`);
+});
