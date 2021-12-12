eaebd6e7c23020d4bb5e8c35e1334b4734f32b36
DTIV <dan_troyer@hotmail.com>
2021-12-12 16:47:50 -0500

    added admin controller

--- src/controller/auth/passportTwitter.ts
+++ src/controller/auth/passportTwitter.ts
@@ -11,7 +11,7 @@ export const intitPassportTwitter = () => {
       {
         consumerKey: process.env.TWITTER_CONSUMER_KEY as string,
         consumerSecret: process.env.TWITTER_CONSUMER_SECRET as string,
-        callbackURL: "http://localhost:3000/",
+        callbackURL: "http://localhost:3000/auth/viteconnect",
       },
       function (token, tokenSecret, profile, cb) {
         console.log(token, tokenSecret, profile, cb);
