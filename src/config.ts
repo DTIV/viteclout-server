f9bbe2ca3b5f9ab285bcf3683d22c4466dfe1e76
DTIV <dan_troyer@hotmail.com>
2021-12-16 12:50:14 -0500

    test contract added

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export default {
   PORT: 5000,
   MONGO_DB_URL: "mongodb://localhost:27017/viteclout",
   RPC_URL: "ws://localhost:23457",
-  CONTRACT_ADDRESS: "vite_f57ea0ba104ec80ea36b47165ff01d42b9468d3028243b6e7c",
+  CONTRACT_ADDRESS: "vite_2697d9e2ef729675e0029e37c55764e068b6ddb83d7d70b170",
 };
